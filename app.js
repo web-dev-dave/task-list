82c805577c3eb31baa28702c9b08a98103c0ee1d
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-12-22 20:01:37 +1300

    Create function to load event listeners

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const taskList = document.querySelector('.collection')
 const clearBtn = document.querySelector('.clear-tasks')
 const filter = document.querySelector('#filter')
 const taskInput = document.querySelector('#task')
+
+//  Load all event listeners
+loadEventListeners()
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask)
+}
