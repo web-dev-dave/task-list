0cefa071bd919b4f0333a0657835ff3b8e52ff43
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-12-22 20:10:22 +1300

    Create addTask function

--- app.js
+++ app.js
@@ -13,3 +13,32 @@ function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask)
 }
+
+// Add Task
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task')
+  }
+
+  // Create li element
+  const li = document.createElement('li')
+  // Add class
+  li.className = 'collection-item'
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value))
+  // Create new link element
+  const link = document.createElement('a')
+  // Add class
+  link.className = 'delete-item secondary-content'
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove></i>'
+  // Append the link to li
+  li.appendChild(link)
+
+  // Append li to ul
+console.log(li)
+  taskList.appendChild(li)
+
+
+  e.preventDefault();
+}
