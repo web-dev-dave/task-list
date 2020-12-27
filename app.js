4d0c3cb1807ae854b4167b00b8367f658c85b60b
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-12-27 13:38:48 +1300

    built function to retrieve tasks from ls

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ loadEventListeners()
 
 // Load all event listeners
 function loadEventListeners() {
+  // DOM Load event
+  document.addEventListener('DOMContentLoaded', getTasks)
   // Add task event
   form.addEventListener('submit', addTask)
   // Remove task event
@@ -18,6 +20,35 @@ function loadEventListeners() {
   clearBtn.addEventListener('click', clearTasks)
   // Filter tasks
   filter.addEventListener('keyup', filterTasks)
+}
+
+// Get Tasks from local storage
+function getTasks() {
+  let tasks;
+
+  localStorage.getItem('tasks') === null
+    ? tasks = []
+    : tasks = JSON.parse(localStorage.getItem('tasks'))
+
+  tasks.forEach(task => {
+    // Create li element
+    const li = document.createElement('li')
+    // Add class
+    li.className = 'collection-item'
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(task))
+    // Create new link element
+    const link = document.createElement('a')
+    // Add class
+    link.className = 'delete-item secondary-content'
+    // Add icon html
+    link.innerHTML = '<i class="fas fa-trash"></i>'
+    // Append the link to li
+    li.appendChild(link)
+
+    // Append li to ul
+    taskList.appendChild(li)
+  })
 }
 
 // Add Task
